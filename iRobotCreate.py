6381a1f2b6dba7daea0ad326df62f181085616f6
Chris Garman <cjg316@lehigh.edu>
2014-08-05 10:13:44 -0400

    fixed minor error

--- iRobotCreate.py
+++ iRobotCreate.py
@@ -305,7 +305,7 @@ class iRobotCreate:
         
         #finds the point at which minimum cost occurs
         #if cost is <.1, it does not move
-        if(dist.flatten().argmin() < .1):
+        if(dist.flatten()[dist.flatten().argmin()] < .1):
             self.setvel(0,0)
             return;
         
