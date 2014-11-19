fixed target calculation.

LilyMasterControl.py
@@ -108,22 +108,20 @@ def follow():
     distF = 1.5 #distance behind person to follow
     
     #find the point 1.5 meters behind the user on a straight line between the user and the robot
-    if not a == 0:
-        c = a - (distF/(1+(b/a)**2))**.5
-        d = b - (b/a)*((distF/(1+(b/a)**2))**.5)
-    else: #user is directly in front of LILI
-        c = 0 
-        d = b - distF 
+    
+    c = a - distF*(a/((a**2+b**2)**0.5))
+    d = b - distF*(b/((a**2+b**2)**0.5))
+    
     
     #stop moving if bump sensor is being pressed, does not stop following
     if not r.isbumped():
         n = lidar.lidarScan(1,0,0)
-        lidar_x = []
-        lidar_y = []
+        lidar_x = [-5]
+        lidar_y = [0]
         for i in range(0,n):
-            if lidar.lidarScan(0,i,0)>=100 and lidar.lidarScan(0,i,0)<=2000:
+            if lidar.lidarScan(0,i,0)>=100 and lidar.lidarScan(0,i,0)<=1500:
                 lidar_x.append(np.cos(((240)*(i/n)-120)*3.1415/180)*lidar.lidarScan(0,i,0)/1000.0+.13)
-                lidar_y.append(-np.sin(((240)*(i/n)-120)*3.1415/180)*lidar.lidarScan(0,i,0)/1000.0)
+                lidar_y.append(np.sin(((240)*(i/n)-120)*3.1415/180)*lidar.lidarScan(0,i,0)/1000.0)
         #line1.set_data(lidar_x,lidar_y)
         #fig.canvas.draw()
         r.goToGoal(c,d,[lidar_x,lidar_y])
@@ -143,6 +141,7 @@ def checkReady():
 #adds command from KinectMonitor onto appropriate queue
 def KinectQueue():
     line = km.line.strip()
+    print line
     if line[:line.find(' ')] == "follow": #all information regarding following starts with the word "follow"
         qFollow.put(line)
     elif line[:line.find(' ')] == "face": #all information regarding faces start with the word "face"
@@ -213,7 +212,7 @@ def faceResponse():
         skeletonPersonIDs[int(parts[2])] = int(parts[3]) #add new user to dictionary
         readyTT = False
         sys.stderr.write("person is " + parts[3] + "\n")
-        if len(indivTime)<int(parts[3]):
+        if len(indivTime)<=int(parts[3]):
             for x in range(len(indivTime),int(parts[3])+1):
                 parts.append(300.0)
             sp.write("hello " + parts[3] + "\n")
@@ -365,7 +364,7 @@ sp.write("query\n")
 
 #start VoiceMonitor listening
 vm.write("start\n")
-
+strike = 0;
 #Waiting state: search for gestures from the kinect monitor
 while quit == False: # The user has not asked to quit.
     vm.tryReadLine()  # receive input from the voice monitor
@@ -376,9 +375,11 @@ while quit == False: # The user has not asked to quit.
     	indivTime[x] = indivTime[x]-0.2
     if not qFollow.empty(): #if there is a command in the qFollow queue
         follow()
+        strike = 0
     else:
+        strike=strike+1
         #stop following if the KinectMonitor stops sending values
-        if state == "following":
+        if state == "following" and strike>2:
             km.write('follow stop\n')
             
 # if there are items on the queue, try to respond

iRobotCreate.py
@@ -302,24 +302,29 @@ class iRobotCreate:
     #Added by CJG
     #trajectory method must be called before use
     def goToGoal(self, xGoal, yGoal, obst):
-    	
+        
+    	#print obst
+        # print xGoal," ",yGoal
         #should be a 6 x 11 x 11 matrix of distances between trajectory path points and the goal point
         dist = ((self.xcoord - xGoal)**2 + (self.ycoord - yGoal)**2)**.5
-        # n = len(obst[0])
-        # obst_dist = np.zeros((6,11,11,n));
-        # for i in range(0,n):
-            # obst_dist[:,:,:,i]=((self.xcoord - obst[0][i])**2 + (self.ycoord - obst[1][i])**2)**.5
+        n = len(obst[0])
+        obst_dist = np.zeros((6,11,11,n))
+        for i in range(0,n):
+            obst_dist[:,:,:,i]=((self.xcoord - obst[0][i])**2 + (self.ycoord - obst[1][i])**2)**.5
         #print obst_dist
         #quit()
         # print obst
         # quit()
-        dist = dist[:,:,10]#+(2000*np.sum(np.sum(obst_dist<.20,3),2))
+        dist = dist[:,:,10]+(2000*np.sum(np.sum(obst_dist<.20,3),2))
+        #print dist
         
-        #finds the point at which minimum cost occurs
-        #if cost is <.1, it does not move
+        index = dist.flatten().argmin()
+
+        #find row and column of minimum value based off of flattened index
+        rowNum = index/11
+        colNum = index % 11
+        # print self.xcoord[rowNum,colNum,10]," ",self.ycoord[rowNum,colNum,10]
         
-        #realized there is strange logic after implementation
-        #left in because this yielded best results from when we tried to fix it
         if(((xGoal)**2 + (yGoal)**2)**.5 < .2):
             
             if abs(self.vel-0)>0.05:
@@ -329,11 +334,7 @@ class iRobotCreate:
             self.setvel(self.vel,0)
             return
         
-        index = dist.flatten().argmin()
         
-        #find row and column of minimum value based off of flattened index
-        rowNum = index/11
-        colNum = index % 11
         mult = 1
         if abs(self.vel-self.v[rowNum])>0.05 and self.v[rowNum]!=0:
             mult = (self.vel+0.1*(self.v[rowNum]-self.vel)/abs(self.v[rowNum]-self.vel))/self.v[rowNum]
@@ -341,7 +342,7 @@ class iRobotCreate:
         #move
         self.setvel(mult*self.v[rowNum],mult*self.omega[colNum]*(self.vel!=0))                
 
-           
+        
 
 
 
@@ -608,7 +609,9 @@ class iRobotCreate_real:
     #Added by CJG
     #trajectory method must be called before use
     def goToGoal(self, xGoal, yGoal, obst):
-    	
+        
+    	#print obst
+        print xGoal," ",yGoal
         #should be a 6 x 11 x 11 matrix of distances between trajectory path points and the goal point
         dist = ((self.xcoord - xGoal)**2 + (self.ycoord - yGoal)**2)**.5
         # n = len(obst[0])
@@ -619,13 +622,16 @@ class iRobotCreate_real:
         #quit()
         # print obst
         # quit()
-        dist = dist[:,:,10]#+(2000*np.sum(np.sum(obst_dist<.20,3),2))
+        dist = dist[:,:,10]#+(2000*np.sum(np.sum(obst_dist<.20,3),2)
+        #print dist
         
-        #finds the point at which minimum cost occurs
-        #if cost is <.1, it does not move
+        index = dist.flatten().argmin()
+
+        #find row and column of minimum value based off of flattened index
+        rowNum = index/11
+        colNum = index % 11
+        print self.xcoord[rowNum,colNum,10]," ",self.ycoord[rowNum,colNum,10]
         
-        #realized there is strange logic after implementation
-        #left in because this yielded best results from when we tried to fix it
         if(((xGoal)**2 + (yGoal)**2)**.5 < .2):
             
             if abs(self.vel-0)>0.05:
@@ -635,17 +641,13 @@ class iRobotCreate_real:
             self.setvel(self.vel,0)
             return
         
-        index = dist.flatten().argmin()
         
-        #find row and column of minimum value based off of flattened index
-        rowNum = index/11
-        colNum = index % 11
         mult = 1
         if abs(self.vel-self.v[rowNum])>0.05 and self.v[rowNum]!=0:
             mult = (self.vel+0.1*(self.v[rowNum]-self.vel)/abs(self.v[rowNum]-self.vel))/self.v[rowNum]
         self.vel = mult*self.v[rowNum]
         #move
-        self.setvel(mult*self.v[rowNum],mult*self.omega[colNum])                
+        self.setvel(mult*self.v[rowNum],mult*self.omega[colNum]*(self.vel!=0))
                  
 
 

lidartest.py
@@ -1,28 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-
+import time
 import ctypes
 lib=ctypes.CDLL('lidar')
 
 lib.lidarScan.restype = ctypes.c_long
 
-plt.ion()
+# plt.ion()
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-line1, = ax.plot([], [], 'r.') # Returns a tuple of line objects, thus the comma
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# line1, = ax.plot([], [], 'r.') # Returns a tuple of line objects, thus the comma
 
-ax.set_ylim([-5000,5000])
-ax.set_xlim([-5000,5000])
-
+# ax.set_ylim([-5000,5000])
+# ax.set_xlim([-5000,5000])
+count = 0
 while 1:
     n = lib.lidarScan(1,0,0)
-    data = np.zeros((1,n))
+    if n<0:
+        print "negative"
+        continue
+    data = np.zeros(n)
     for i in range(0,n):
-        data[0][i]=lib.lidarScan(0,i,0)
+        data[i]=lib.lidarScan(0,i,0)
     th = np.arange(n)*240.0/n-120
-    x=data*np.cos(th*3.1415/180)
-    y=data*np.sin(th*3.1415/180)
-    line1.set_data(x,y)
-    fig.canvas.draw()
+    x=np.multiply(data,np.cos(th*3.1415/180))
+    y=np.multiply(data,np.sin(th*3.1415/180))
+    print x[0]
+    print count
+    count = count +1
+    # line1.set_data(x,y)
+    # fig.canvas.draw()
+    time.sleep(0.1)