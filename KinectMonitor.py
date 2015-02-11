7382f87a7cf8020e4fd3e55a0a44f697d3869c9c
deadtomgc <deadtomgc@gmail.com>
2015-02-11 14:49:34 -0500

    added recognition by proportions

--- KinectMonitor.py
+++ KinectMonitor.py
@@ -33,6 +33,10 @@ lib.loop(track)
 MAX_GUESSES = 30 #maximum number of guesses the face identifier is allowed before a person is considered unrecognized
 
 def detect_motion():
+    global curSkeletonPersonIDs
+    global oldSkeletonPersonIDs
+    global aspects = [[0,-1,-1,-1]] #person id,hips-head dist,sh dist,sh-elbow
+    global skasps = [[0,-1,-1,-1]]  #skeleton id, ...
     global rightWave
     global leftWave
     global follow
@@ -155,8 +159,81 @@ def detect_motion():
                 quitstage[user]="none"#invalid for the quit gesture
             if lib.getUserSkeletonR_HandY(track,user)-lib.getUserSkeletonHeadY(track,user)>0 or lib.getUserSkeletonR_HandConf(track,user)<=0.5:
                 quitstage[user]="none"#invalid for the quit gesture
-        
+            found = False
+            i = 0
+            for skasp in skasps:
+                if skasp[0] == user:
+                    found = True
+                    break
+                i=i+1
+            if found==False:
+                skasps.append([user,-1,-1,-1])
+            
+            if lib.getUserSkeletonHeadConf(track,user)>0.5 and lib.getUserSkeletonR_HipConf(track,user)>0.5:
+                
+                #hip to head
+                skasps[i][1] = (sqrt((lib.getUserSkeletonHeadX(track,user)-lib.getUserSkeletonR_HipX(track,user))**2 + (lib.getUserSkeletonHeadY(track,user)-lib.getUserSkeletonR_HipY(track,user))**2 + (lib.getUserSkeletonHeadZ(track,user)-lib.getUserSkeletonR_HipZ(track,user))**2) + sqrt((lib.getUserSkeletonHeadX(track,user)-lib.getUserSkeletonL_HipX(track,user))**2 + (lib.getUserSkeletonHeadY(track,user)-lib.getUserSkeletonY_HipY(track,user))**2 + (lib.getUserSkeletonHeadZ(track,user)-lib.getUserSkeletonL_HipZ(track,user))**2))/2 #avg dist from hip to head
+        			
+                #sh dist
+                skasps[i][2] = sqrt((lib.getUserSkeletonL_ShX(track,user)-lib.getUserSkeletonR_ShX(track,user))**2 + (lib.getUserSkeletonL_ShY(track,user)-lib.getUserSkeletonR_ShY(track,user))**2 + (lib.getUserSkeletonL_ShZ(track,user)-lib.getUserSkeletonR_ShZ(track,user))**2)
+                #upper arm dist
+                skasps[i][3] = sqrt((lib.getUserSkeletonL_ShX(track,user)-lib.getUserSkeletonR_ElbowX(track,user))**2 + (lib.getUserSkeletonL_ShY(track,user)-lib.getUserSkeletonR_ElbowY(track,user))**2 + (lib.getUserSkeletonL_ShZ(track,user)-lib.getUserSkeletonR_ElbowZ(track,user))**2)
+                
+                if not lib.getUserID(track, user) in curSkeletonPersonIDs:
+                	curSkeletonPersonIDs[lib.getUserID(track, user)] = -1
+                if curSkeletonPersonIDs[lib.getUserID(track, user)]<0:
+                    for j in range(0,len(aspects)):#check for similarity
+                        if skasps[i][1] < aspects[j][1]*1.02 and skasps[i][1] > aspects[j][1]*0.98:
+                            if skasps[i][2] < aspects[j][2]*1.02 and skasps[i][2] > aspects[j][2]*0.98:
+                                if skasps[i][3] < aspects[j][3]*1.02 and skasps[i][3] > aspects[j][3]*0.98:
+                                    #TODO: send notification of recognition and set appropriate values
+                                    curSkeletonPersonIDs[lib.getUserID(track, user)] = aspects[0]
+                                    p.write("face recognized " + str(lib.getUserID(track, user)) + " " + str(curSkeletonPersonIDs[lib.getUserID(track, user)]) + " " + str(time.time()) + "\n")
+                                    sys.stderr.write("Proportions match! Skeleton " + str(lib.getUserID(track, user)) + " identified as person: " + str(curSkeletonPersonIDs[lib.getUserID(track, user)]) + "\n")
+                    
 
+            
+               
+            
+            #keys are skeleton id's (not indexies)
+        for key in curSkeletonPersonIDs.keys():
+        	if curSkeletonPersonIDs[key]>=0:
+        		found = False
+        		i = 0
+        		for aspect in aspects:
+        			if aspect[0] == curSkeletonPersonIDs[key]:
+        				found = True
+        				break
+        			i=i+1
+        		if found==False:
+        			aspects.append([curSkeletonPersonIDs[key],-1,-1,-1])
+        		found = False
+        		user = 0
+        		for i in range(0,lib.getUserCount(track)):
+        			if lib.getUserID(track, i) == key:
+        				user = i
+        				found = True
+        				break
+        		if found==False:
+        			continue #the skeleton of this person isn't present
+        		if lib.getUserSkeletonHeadConf(track,user)>0.5 and lib.getUserSkeletonR_HipConf(track,user)>0.5:
+        			#TODO check distance and average it
+        			if aspects[i][1] < 0:
+        				aspects[i][1] = (sqrt((lib.getUserSkeletonHeadX(track,user)-lib.getUserSkeletonR_HipX(track,user))**2 + (lib.getUserSkeletonHeadY(track,user)-lib.getUserSkeletonR_HipY(track,user))**2 + (lib.getUserSkeletonHeadZ(track,user)-lib.getUserSkeletonR_HipZ(track,user))**2) + sqrt((lib.getUserSkeletonHeadX(track,user)-lib.getUserSkeletonL_HipX(track,user))**2 + (lib.getUserSkeletonHeadY(track,user)-lib.getUserSkeletonY_HipY(track,user))**2 + (lib.getUserSkeletonHeadZ(track,user)-lib.getUserSkeletonL_HipZ(track,user))**2))/2 #avg dist from hip to head
+        			else:
+        				aspects[i][1] = (aspects[i][1]+(sqrt((lib.getUserSkeletonHeadX(track,user)-lib.getUserSkeletonR_HipX(track,user))**2 + (lib.getUserSkeletonHeadY(track,user)-lib.getUserSkeletonR_HipY(track,user))**2 + (lib.getUserSkeletonHeadZ(track,user)-lib.getUserSkeletonR_HipZ(track,user))**2) + sqrt((lib.getUserSkeletonHeadX(track,user)-lib.getUserSkeletonL_HipX(track,user))**2 + (lib.getUserSkeletonHeadY(track,user)-lib.getUserSkeletonY_HipY(track,user))**2 + (lib.getUserSkeletonHeadZ(track,user)-lib.getUserSkeletonL_HipZ(track,user))**2))/2)/2 #avg dist from hip to head
+        			if aspects[i][2] < 0: #sh dist
+        				aspects[i][2] = sqrt((lib.getUserSkeletonL_ShX(track,user)-lib.getUserSkeletonR_ShX(track,user))**2 + (lib.getUserSkeletonL_ShY(track,user)-lib.getUserSkeletonR_ShY(track,user))**2 + (lib.getUserSkeletonL_ShZ(track,user)-lib.getUserSkeletonR_ShZ(track,user))**2)
+        			else:
+        				aspects[i][2] = (aspects[i][2]+sqrt((lib.getUserSkeletonL_ShX(track,user)-lib.getUserSkeletonR_ShX(track,user))**2 + (lib.getUserSkeletonL_ShY(track,user)-lib.getUserSkeletonR_ShY(track,user))**2 + (lib.getUserSkeletonL_ShZ(track,user)-lib.getUserSkeletonR_ShZ(track,user))**2))/2
+        			if aspects[i][3] < 0: #sh dist
+        				aspects[i][3] = sqrt((lib.getUserSkeletonL_ShX(track,user)-lib.getUserSkeletonR_ShX(track,user))**2 + (lib.getUserSkeletonL_ShY(track,user)-lib.getUserSkeletonR_ShY(track,user))**2 + (lib.getUserSkeletonL_ShZ(track,user)-lib.getUserSkeletonR_ShZ(track,user))**2)
+        			else:
+        				aspects[i][3] = (aspects[i][3]+sqrt((lib.getUserSkeletonL_ShX(track,user)-lib.getUserSkeletonR_ShX(track,user))**2 + (lib.getUserSkeletonL_ShY(track,user)-lib.getUserSkeletonR_ShY(track,user))**2 + (lib.getUserSkeletonL_ShZ(track,user)-lib.getUserSkeletonR_ShZ(track,user))**2))/2
+        			
+# TODO Allow identification to be done using body proportion and color information
+# when confidence is high that the user is the correct person, send identifier.
+#LMC should be modified to look through current users and delete false recognitions when new recognitions are received 
 def facialActions():
     global curSkeletonPersonIDs
     global oldSkeletonPersonIDs
@@ -191,6 +268,7 @@ def facialActions():
                         #person is now recognized
                         p.write("face recognized " + str(key) + " " + str(curSkeletonPersonIDs[key]) + " " + str(time.time()) + "\n")
                         personIDAttempts[key] = MAX_GUESSES + 1
+                        
                         sys.stderr.write("recognized skeleton: " + str(key) + " as person: " + str(curSkeletonPersonIDs[key]) + "\n")
                     elif curSkeletonPersonIDs[key] < 0 and oldSkeletonPersonIDs[key] >= 0:
                         #recognized person has left the frame
